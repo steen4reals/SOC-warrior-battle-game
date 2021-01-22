912b60b92b3d5004fa9783d8308e96aab9e20517
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-01-22 15:53:30 +0000

    created HandleAttach

--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace WarriorBattle
         static void Main(string[] args)
         {
             Console.WriteLine("FIGHT!");
+
+            Warrior warrior1 = new Warrior("Fred", 15, 2, 6);
+            Warrior warrior2 = new Warrior("Tom", 20, 4, 2);
+
+            HandleAttack(warrior1, warrior2);
+            Console.WriteLine(warrior1.Health);
+            Console.WriteLine(warrior2.Health);
+
+
         }
+        static void HandleAttack(Warrior warriorA, Warrior warriorB)
+        {
+            int warriorAattack = warriorA.GetAttack();
+            int warriorBblock = warriorB.GetBlock();
+            int warriorBdamage = warriorAattack - warriorBblock;
+            if (warriorBdamage > 0)
+            {
+                warriorB.ApplyDamage(warriorBdamage);
+            }
+        }
+
+
+
     }
 }
--- Warrior.cs
+++ Warrior.cs
@@ -13,4 +13,20 @@ public Warrior(string name, int health, int maxAttack, int maxBlock){
     this.MaxBlock = maxBlock;
 }
 
+public int GetAttack(){
+    var random = new Random();
+    int randomNum = random.Next(0, MaxAttack);
+    return randomNum;
+}
+
+public int GetBlock(){
+    var random = new Random();
+    int randomNum = random.Next(0, MaxBlock);
+    return randomNum;
+}
+
+public void ApplyDamage( int damage){
+    Health = Health - damage;
+}
+
 }
